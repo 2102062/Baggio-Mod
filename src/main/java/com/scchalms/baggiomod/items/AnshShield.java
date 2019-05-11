71ab07de3ab5a82062e5a9e59652cd16fb8e1ac5
2102062 <2102062@geneva304.org>
2019-05-11 13:03:31 -0500

    ansh

---
+++ src/main/java/com/scchalms/baggiomod/items/AnshShield.java
@@ -0,0 +1,12 @@
+package com.scchalms.baggiomod.items;
+
+import com.scchalms.baggiomod.BaggioMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemShield;
+
+public class AnshShield extends ItemShield {
+    public static final String id = "ansh_shield";
+    public AnshShield(){
+        setCreativeTab(BaggioMod.BAGGIO_TAB);
+    }
+}
